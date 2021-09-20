f3a11c77c33766fb350b5707cba17ec5338af65d
Ahmed Shire <88110801+ahmed27shire@users.noreply.github.com>
2021-09-19 21:45:31 -0400

    Add files via upload

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ ✅ ✅   Assertion passed: ${actual} === ${expected}");
+  } else {
+    console.log("🛑 🛑 🛑  Assertion failed: ${actual} !== ${expected}");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+eqArrays([1, 2, 3], [1, 2, 3])
