d77308e680f16a5a3269daa06064696ec32a3319
Ahmed Shire <88110801+ahmed27shire@users.noreply.github.com>
2021-09-20 14:39:06 -0400

    Update tail.js

--- tail.js
+++ tail.js
@@ -22,3 +22,26 @@ assertEqual(result.toString(), ["Lighthouse", "Labs"].toString());
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3);
+
+  **NEW**
+    
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ ✅ ✅   Assertion passed: ${actual} === ${expected}");
+  } else {
+    console.log("🛑 🛑 🛑  Assertion failed: ${actual} !== ${expected}");
+  }
+};
+
+const tail = function(array) {
+  let outputArray = [];
+  for (let index = 1; index < array.length; index++) {
+    const element = array[index];
+    outputArray.push(element);
+          
+  }
+  return outputArray;
+};
+
+module.exports = tail;
+
